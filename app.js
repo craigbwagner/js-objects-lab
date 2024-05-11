0279eba74761d87c86f22a1307fe3daeede60224
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-05-11 18:06:46 -0400

    completed exercise 11

--- app.js
+++ app.js
@@ -126,6 +126,24 @@ pokemon.forEach(p => {
 	}
 })
 
+/*
+Exercise 11
+1. Add a method called `catchPokemon` to the `game` object. This method should:
+- Accept an object as a parameter called `pokemonObj`
+- Add the `pokemonObj` to the `game.party` array.
+- not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 11 here:
+*/
+
+game.party.catchPokemon = function(pokemonObj) {
+	game.party.push(pokemonObj);
+}
+
+game.party.catchPokemon(pokemon[28]);
+
 // console.dir(pokemon, { maxArrayLength: null });
 console.log(game);
 
